b10c63dde0cc31709d5f23cac147deda6714973e
Igor Paes <irsp2121@gmail.com>
2025-06-15 12:20:36 -0300

    Aplicar .gitignore e limpar arquivos não desejados

--- Documents/Front-end-JMT/Exercicios_Semanais_JMT/M2S2/M2S2_exercicios/ex4_tela_cadastro_celular/scripts/cadastro-celulares.js
+++
@@ -1,5 +0,0 @@
-document.getElementById("form-celular").addEventListener("submit", function(event) {
-    event.preventDefault();
-    document.getElementById("mensagem").textContent = "Celular cadastrado com sucesso!";
-    this.reset();
-});
